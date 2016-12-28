6bd59a389f7a45753d1f629c1e1a9380aff12b0b
Mohammad AbuShady <mohammad@abushady.com>
2016-12-29 01:26:58 +0200

    Load the posts from ajax

--- src/MainContent.js
+++ src/MainContent.js
@@ -1,48 +1,59 @@
 import React, { Component } from 'react';
 import PostPreview from './PostPreview';
+import $ from 'jquery'
 
 class MainContent extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      comments: []
+    }
+  }
+
+  _getPosts() {
+    $.ajax({
+      method: 'GET',
+      url: 'http://blog-api-test.abushady.com/posts',
+      success: (comments) => {
+        this.setState({ comments })
+      }
+    });
+  }
+
+  componentWillMount() {
+    this._getPosts();
+  }
+
   render() {
     return(
-    <div className="container">
+      <div className="container">
         <div className="row">
-            <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-              <PostPreview
-                post_url='my_post.html'
-                title='This is finally a working post'
-                subtitle='And this is the subtitle for the working post'
-                author_url='some_author.html'
-                post_date='February 5, 2016'
-              />
-              <PostPreview
-                post_url='my_post.html'
-                title="I believe every human has a finite number of heartbeats. I don't intend to waste any of mine."
-                author_url='some_author.html'
-                post_date='February 10, 2016'
-              />
-              <PostPreview
-                post_url='my_post.html'
-                title='Science has not yet mastered prophecy'
-                subtitle='We predict too much for the next year and yet far too little for the next ten.'
-                author_url='some_author.html'
-                post_date='February 15, 2016'
-              />
-              <PostPreview
-                post_url='my_post.html'
-                title='Failure is not an option.'
-                subtitle='Many say exploration is part of our destiny, but it’s actually our duty to future generations.'
-                author_url='some_author.html'
-                post_date='February 15, 2016'
-              />
-              {/* Pager */}
-                <ul className="pager">
-                    <li className="next">
-                        <a href="#">Older Posts &rarr;</a>
-                    </li>
-                </ul>
-            </div>
+          <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+            {
+              this.state.comments.map((comment) => {
+                return(
+                  <PostPreview
+                    post_url='my_post.html'
+                    title={comment.title}
+                    subtitle='And this is the subtitle for the working post'
+                    author_url='some_author.html'
+                    post_date='February 5, 2016'
+                    key={"post-"+comment.uuid}
+                  />
+                )
+              })
+            }
+            {/* Pager */}
+
+            <ul className="pager">
+              <li className="next">
+                <a href="#">Older Posts &rarr;</a>
+              </li>
+            </ul>
+          </div>
         </div>
-    </div>
+      </div>
     )
   }
 }
